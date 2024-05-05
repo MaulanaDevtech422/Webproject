a5e22f0a6a5a068357f7166e19316d74cb4e9cb9
Ikrommaulana <163699226+MaulanaDevtech422@users.noreply.github.com>
2024-05-05 15:54:00 +0700

    Update about.js

--- about.js
+++ about.js
@@ -35,14 +35,14 @@ $(document).ready(function() {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Web Developer", "Blogger", "Designer", "Freelancer",],
+    strings: ["Web Developer", "Blogger", "Web Design", "Freelancer",],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
   });
 
   var typed = new Typed(".typing-2", {
-    strings: ["Web Developer", "Blogger", "Designer", "Freelancer"],
+    strings: ["Web Developer", "Blogger", "Web Design", "Freelancer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
