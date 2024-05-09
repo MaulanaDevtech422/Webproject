11a0a41d74f5023641bbd32f56867618554d7097
Ikrommaulana <163699226+MaulanaDevtech422@users.noreply.github.com>
2024-05-09 07:10:40 +0700

    Add files via upload

---
+++ grt.js
@@ -0,0 +1,9 @@
+function generatePassword() {
+  const length = document.getElementById("length").value;
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()=%_?";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    password += charset.charAt(Math.floor(Math.random() * charset.length));
+  }
+  document.getElementById("password").value = password;
+}
